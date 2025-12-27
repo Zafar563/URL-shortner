3967043785b9a57cb583dd3be69ae0c44f254fdb
Zafar563 <zafartohirov306@gmail.com>
2025-12-27 12:17:51 +0500

    Initial commit

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,7 +10,7 @@ import (
 
 type Config struct {
 	Env         string `yaml:"env" env-default:"local"`
-	StoragePath string `yaml:"storage_path" env-required:"true"`
+	StoragePath string `yaml:"storage_path" env:"STORAGE_PATH" env-required:"true"`
 	HTTPServer  `yaml:"http_server"`
 }
 
